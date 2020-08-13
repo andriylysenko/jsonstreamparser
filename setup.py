2b480ac2617dfc3cb6f65794c5d9f18a2453fd9b
Andrey Lysenko <andrey.lysenko@gmail.com>
2020-08-13 11:10:09 -0400

    add license

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-# from distutils.core import setup, Extension
-from setuptools import setup, Extension, Command
+from setuptools import setup, Extension
 import setuptools
 
 stream_json_parser_utils = Extension('stream_json_parser_utils', extra_compile_args = [ '--std=c++11' ], sources = [ 'utils/src/utils.cpp' ])
 
 if __name__ == '__main__':
    setup(
-       name = 'streamjsonparser',
-       version = '1.0',
-       description = 'Stream Json Parser',
-       ext_modules = [stream_json_parser_utils],
+       name='streamjsonparser',
+       version='1.1',
+       description='Stream Json Parser',
+       ext_modules=[stream_json_parser_utils],
        author='Andrey Lysenko',
        author_email='andrey.lysenko@gmail.com',
        packages=setuptools.find_packages(),
        include_package_data=True,
-       zip_safe=False,
-       python_requires='>=3.4'
+       zip_safe=False
    )
